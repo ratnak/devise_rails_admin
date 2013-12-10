ad22717a53200a1f7a1026404a8d3bf40f48b77a
ratnak <ratna.v143@gmail.com>
2013-12-10 22:04:43 +0530

    devise and rails admin installed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JvPitcher::Application.config.secret_token = 'ca8f64b3f262ee6c6a96869f12ea15ecb45ea637b00b8f43ae2cf40e147256d20597487998e3c8f5a596888faf91485455ea3a09ea8f3ae249b1e4a6eb49bc27'
